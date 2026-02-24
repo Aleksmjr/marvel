934cb1bdc4f6b7404d3cf6ad29625ddd0a46303f
«Aleksei <Aleksmjr97@gmail.com>
2026-02-24 22:11:35 +0300

    Update: created RandomChar component with Marvel API integration and added MarvelService for data fetching

---
+++ src/services/MarvelService.js
@@ -0,0 +1,43 @@
+class MarvelService {
+
+  _apiBase = process.env.REACT_APP_API_BASE;
+  _apiKey = process.env.REACT_APP_API_KEY;
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Ошибка получения данных с ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}/characters?${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+
+    const description = char.description
+    ? char.description.slice(0, 150) + (char.description.length > 150 ? '...' : '')
+    : 'There is no description for this character';
+
+    return {
+      name: char.name,
+      description: description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension, 
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+
+}
+
+export default MarvelService;
