63112b5c92151f348ee271382331554b4319abfb
«Aleksei <Aleksmjr97@gmail.com>
2026-03-05 23:36:37 +0300

    Update: refactor components to use class-based structure; implement character selection and loading/error handling

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,58 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+  };
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService
+      .getLimitCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({
+      charList,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
+
+  render() {
+    const { charList } = this.state;
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className="char__list">
+        <ul className="char__grid">
+          {charList.map((char) => (
+            <li
+              key={char.id}
+              onClick={() => this.props.onCharSelected(char.id)}
+              className="char__item"
+            >
+              <img src={char.thumbnail} alt="abyss" />
+              <div className="char__name">{char.name}</div>
+            </li>
+          ))}
+        </ul>
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,4 @@
 class MarvelService {
-
   _apiBase = process.env.REACT_APP_API_BASE;
   _apiKey = process.env.REACT_APP_API_KEY;
 
@@ -7,37 +6,51 @@ class MarvelService {
     let res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error(`Ошибка получения данных с ${url}, status: ${res.status}`);
+      throw new Error(
+        `Ошибка получения данных с ${url}, status: ${res.status}`,
+      );
     }
 
     return await res.json();
-  }
+  };
 
   getAllCharacters = async () => {
-    const res = await this.getResource(`${this._apiBase}/characters?${this._apiKey}`);
+    const res = await this.getResource(
+      `${this._apiBase}/characters?${this._apiKey}`,
+    );
     return res.data.results.map(this._transformCharacter);
-  }
+  };
+
+  getLimitCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBase}/characters?limit=9&${this._apiKey}`,
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
 
   getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    const res = await this.getResource(
+      `${this._apiBase}/characters/${id}?${this._apiKey}`,
+    );
     return this._transformCharacter(res.data.results[0]);
-  }
+  };
 
   _transformCharacter = (char) => {
-
     const description = char.description
-    ? char.description.slice(0, 150) + (char.description.length > 150 ? '...' : '')
-    : 'There is no description for this character';
+      ? char.description.slice(0, 150) +
+        (char.description.length > 150 ? '...' : '')
+      : 'There is no description for this character';
 
     return {
+      id: char.id,
       name: char.name,
       description: description,
-      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension, 
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
-      wiki: char.urls[1].url
-    }
-  }
-
+      wiki: char.urls[1].url,
+      comics: char.comics.items,
+    };
+  };
 }
 
 export default MarvelService;
